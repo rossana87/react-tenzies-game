dc68584cfea469adb5960a609bd9cfed1d4cfb2a
Rossana87 <rossana.ventrella@gmail.com>
2023-08-09 11:01:58 +0100

    winning part

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 
 const App = () => {
+  
+  const [tenzies, setTenzies] = useState(false)
+
+  const generateNewDie = () => {
+    return {
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false,
+      id: nanoid(),
+    }
+  }
 
   const allNewDice = () => {
     const diceArray = []
     for (let i = 0; i < 10; i++) {
-      diceArray.push({
-        value: Math.ceil(Math.random() * 6), 
-        isHeld: false,
-        id: nanoid(),
-      })
+      diceArray.push(generateNewDie())
     } 
     return diceArray
   }
 
   const [dice, setDice] = useState(allNewDice())
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log('You won')
+    }
+  }, [dice])
   
   const rollDice = () => {
-    setDice(allNewDice())
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
   }
 
   const holdDice = (id) => {
@@ -37,10 +58,18 @@ const App = () => {
 
   return (
     <main className='container'>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='die-board'>
         {diceElements}
       </div>
-      <button className='roll-dice' onClick={rollDice}>Roll</button>
+      <button 
+        className='roll-dice' 
+        onClick={rollDice}
+      >
+        {tenzies ? 'New Game' : 'Roll'}
+      </button>
     </main>
   )
 }
